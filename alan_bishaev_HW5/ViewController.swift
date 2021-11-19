005e67443fa74f3a6a5641d0955fbbcec3d6518a
Egor <georgefed@yandex-team.ru>
2021-11-19 14:08:10 +0300

    Initial Commit

---
+++ alan_bishaev_HW5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alan_bishaev_HW5
+//
+//  Created by Georg on 19.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
