19e2756277630cc8e0b03f01a025c46dae2fc6dd
Egor <georgefed@yandex-team.ru>
2021-11-20 21:14:07 +0300

    moves myButton1

---
+++ Libs/MyButton1/MyButton1/MyButton1.h
@@ -0,0 +1,18 @@
+//
+//  MyButton1.h
+//  MyButton1
+//
+//  Created by Georg on 19.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyButton1.
+FOUNDATION_EXPORT double MyButton1VersionNumber;
+
+//! Project version string for MyButton1.
+FOUNDATION_EXPORT const unsigned char MyButton1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyButton1/PublicHeader.h>
+
+
---
+++ Libs/MyButton1/MyButton1/MyButton1.swift
@@ -0,0 +1,13 @@
+//
+//  MyButton1.swift
+//  MyButton1
+//
+//  Created by Georg on 19.11.2021.
+//
+
+import Foundation
+public struct MyButton1 {
+public static func log(_ s: String) {
+print("MyLogger1 from framework (\(Date())): \(s)")
+}
+}
